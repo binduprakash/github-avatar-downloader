2fa1fb687c74e62e3ec66d2c6b33c5abcfc53d3a
Bindu Prakash <bindu2305@gmail.com>
2019-02-12 07:55:08 +0000

    step 6 completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,18 @@
 var request = require('request');
+var secret = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  headers: {
+    'User-Agent':'request'
+  }
+};
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
